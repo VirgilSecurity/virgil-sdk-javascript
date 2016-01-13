c93c2212d8990a7abcacd4af1026d681ecd79874
boo1ean <boo1ean0807@gmail.com>
2016-01-13 20:33:35 +0200

    Add destroy method to virgil cards client

--- src/virgil-cards/index.js
+++ src/virgil-cards/index.js
@@ -11,6 +11,7 @@ module.exports = function createAPIClient (applicationToken, opts) {
 
 		methods: {
 			create: 'post /virgil-card',
+			destroy: 'delete /virgil-card/{virgil_card_id}',
 			trust: 'post /virgil-card/{virgil_card_id}/actions/sign',
 			untrust: 'post /virgil-card/{virgil_card_id}/actions/unsign',
 			search: 'post /virgil-card/actions/search',
@@ -23,12 +24,14 @@ module.exports = function createAPIClient (applicationToken, opts) {
 
 		transformRequest: {
 			create: create,
+			destroy: untrust,
 			trust: trust,
 			untrust: untrust
 		},
 
 		body: {
 			create: ['public_key_id', 'public_key', 'identity', 'data'],
+			destroy: ['identity'],
 			trust: ['signed_virgil_card_id', 'signed_digest'],
 			unsign: ['signed_virgil_card_id'],
 			search: ['value', 'type', 'relations', 'include_unconfirmed'],
@@ -36,6 +39,7 @@ module.exports = function createAPIClient (applicationToken, opts) {
 		},
 
 		required: {
+			destroy: ['identity'],
 			trust: ['signed_virgil_card_id', 'signed_virgil_card_hash', 'private_key', 'virgil_card_id'],
 			untrust: ['signed_virgil_card_id', 'private_key', 'virgil_card_id'],
 			search: ['value', 'type'],
