b7b2b87fb42327cc00976fa97333e95b85a26bcf
Vadim Avdeev <v.avdeiev@gmail.com>
2017-02-28 14:52:08 +0200

    fix VirgilCard import bug

--- tests/spec/card-manager.js
+++ tests/spec/card-manager.js
@@ -40,3 +40,52 @@ test('create global application card', function (t) {
 		'identity type is set correctly');
 	t.end();
 });
+
+test('export card', function (t) {
+	var fixture = setup();
+	var ownerKeyStub = {
+		sign: sinon.stub().returns(new Buffer('public_key')),
+		exportPublicKey: sinon.stub().returns(new Buffer('public_key'))
+	};
+
+	var cardManager = createCardManager(fixture.context);
+	var card = cardManager.createGlobal(
+		'my_card',
+		ownerKeyStub,
+		IdentityType.EMAIL
+	);
+
+	var exportedCard = card.export();
+	t.doesNotThrow(function () {
+		new Buffer(exportedCard, 'base64');
+	}, 'exports card as base64-encoded string');
+	t.doesNotThrow(function () {
+		JSON.parse(new Buffer(exportedCard, 'base64').toString('utf-8'));
+	}, 'exports card as base64-encoded JSON');
+
+	t.end();
+});
+
+test('import card', function (t) {
+	var fixture = setup();
+	var ownerKeyStub = {
+		sign: sinon.stub().returns(new Buffer('public_key')),
+		exportPublicKey: sinon.stub().returns(new Buffer('public_key'))
+	};
+
+	var cardManager = createCardManager(fixture.context);
+	var card = cardManager.createGlobal(
+		'my_card',
+		ownerKeyStub,
+		IdentityType.EMAIL
+	);
+
+	var exportedCard = card.export();
+
+	var importedCard = cardManager.import(exportedCard);
+
+	t.ok(importedCard, 'imports exported card');
+	t.deepEqual(importedCard, card,
+		'imported card is identical to the exported');
+	t.end();
+});
