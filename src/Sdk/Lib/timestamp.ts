0238dab42190e5fc29f1d4a64ccfad27834ba72b
Vadim Avdeiev <v.avdeiev@gmail.com>
2018-05-29 12:24:28 +0300

    feat: add CachingJwtProvider implementation

--- src/Sdk/Lib/timestamp.ts
+++ src/Sdk/Lib/timestamp.ts
@@ -1,3 +1,12 @@
+/**
+ * Converts javascript date object or timestamp in milliseconds
+ * to Unix timestamp.
+ *
+ * @hidden
+ *
+ * @param {Date | number} date - The date or timestamp to convert.
+ * @returns {number}
+ */
 export function getUnixTimestamp(date: Date|number): number {
 	let time;
 	if (typeof date === 'number') {
@@ -6,4 +15,22 @@ export function getUnixTimestamp(date: Date|number): number {
 		time = date.getTime();
 	}
 	return Math.floor(time / 1000);
+}
+
+/**
+ * Adds the given number of seconds to the given date.
+ *
+ * @hidden
+ *
+ * @param {Date | number} date - The date to add seconds to.
+ * If `date` is a `number` it is treated as a timestamp in milliseconds.
+ * @param {number} seconds - The number of seconds to add.
+ * @returns {Date} - The new date.
+ */
+export function addSeconds (date: Date|number, seconds: number): Date {
+	if (typeof date === 'number') {
+		return new Date(date + seconds * 1000);
+	}
+
+	return new Date(date.getTime() + seconds * 1000);
 }
