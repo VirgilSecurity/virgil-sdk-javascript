e80e637435991cad37f5a22f1634bdbc76b7d4e5
Vadim Avdeiev <v.avdeiev@gmail.com>
2018-07-20 14:19:11 +0300

    deprecate KeyStorage

--- src/Sdk/Lib/KeyStorage/KeyStorage.ts
+++ src/Sdk/Lib/KeyStorage/KeyStorage.ts
@@ -3,8 +3,19 @@ import { IStorageAdapter, IStorageAdapterConfig } from './adapters/IStorageAdapt
 import { PrivateKeyExistsError } from './PrivateKeyExistsError';
 
 export interface IKeyStorageConfig {
+	/**
+	 * Path to folder where to store the data (Node.js only).
+	 */
 	dir?: string;
+
+	/**
+	 * Name of the IndexedDB database where to store the data (Browsers only).
+	 */
 	name?: string;
+
+	/**
+	 * The {@link IStorageAdapter} implementation to be used as a storage backend.
+	 */
 	adapter?: IStorageAdapter;
 }
 
@@ -13,28 +24,60 @@ const DEFAULTS: IStorageAdapterConfig = {
 	name: 'VirgilKeys'
 };
 
+/**
+ * Class representing a storage container for private key data.
+ * Use this class if you need to load the keys stored with
+ * version 4.x of this library. For new code, use the
+ * {@link PrivateKeyStorage} instead.
+ *
+ * @deprecated since version 5.0
+ */
 export class KeyStorage {
 	private adapter: IStorageAdapter;
 
 	constructor (config: IKeyStorageConfig | string = {}) {
+		console.log('Warning! `KeyStorage` is deprecated. Use `PrivateKeyStorage` instead.');
+
 		this.adapter = resolveAdapter(config);
 	}
 
+	/**
+	 * Checks whether a private key data with the given name exist in persistent storage.
+	 * @param {string} name - Name to check.
+	 * @returns {Promise<boolean>} - True if key data exist, otherwise false.
+	 */
 	exists(name: string): Promise<boolean> {
 		validateName(name);
 		return this.adapter.exists(name);
 	}
 
+	/**
+	 * Loads the private key data by the given name.
+	 * @param {string} name - Name of key data to load.
+	 * @returns {Promise<Buffer | null>} - Private key data as Buffer,
+	 * or null if there is no data for the given name.
+	 */
 	load(name: string): Promise<Buffer | null> {
 		validateName(name);
 		return this.adapter.load(name);
 	}
 
+	/**
+	 * Removes the private key data stored under the given name from persistent storage.
+	 * @param {string} name - Name of the key data to remove.
+	 * @returns {Promise<boolean>} - True if the key has been removed, otherwise false.
+	 */
 	remove(name: string): Promise<boolean> {
 		validateName(name);
 		return this.adapter.remove(name);
 	}
 
+	/**
+	 * Persists the private key data under the given name.
+	 * @param {string} name - Name of the key data.
+	 * @param {Buffer} data - The key data.
+	 * @returns {Promise<void>}
+	 */
 	save(name: string, data: Buffer): Promise<void> {
 		validateName(name);
 		validateData(data);
