8517057911220ccc59f3fa02b96a58cdd5133a1a
Vadim Avdeev <v.avdeiev@gmail.com>
2018-12-07 19:42:30 +0200

    cleanup

--- src/__tests__/integration/data/index.ts
+++ src/__tests__/integration/data/index.ts
@@ -1,3 +1,3 @@
 import compatData_ from './data.json';
 // fancy way to import json that works in both node.js and the browser
-export const compatData = compatData_; // as { [key: string]: string };
+export const compatData = compatData_;
