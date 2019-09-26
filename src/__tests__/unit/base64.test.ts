95cd7cadcd86a3fe0d23479b20ec2056655c438d
Serhii Nanovskyi <serhii.nanovskyi@icloud.com>
2019-09-26 15:21:32 +0300

    Fix

--- src/__tests__/unit/base64.test.ts
+++ src/__tests__/unit/base64.test.ts
@@ -1,33 +1,31 @@
-import { expect } from 'chai';
-
 import { base64Encode, base64Decode, base64UrlEncode, base64UrlDecode } from '../../Lib/base64';
 
 describe('base64', () => {
 	describe('base64Encode', () => {
 		it('works', () => {
 			const result = base64Encode('value');
-			expect(result).to.equal('dmFsdWU=');
+			assert.equal(result, 'dmFsdWU=');
 		});
 	});
 
 	describe('base64Decode', () => {
 		it('works', () => {
 			const result = base64Decode('dmFsdWU=');
-			expect(result).to.equal('value');
+			assert.equal(result, 'value');
 		});
 	});
 
 	describe('base64UrlEncode', () => {
 		it('works', () => {
 			const result = base64UrlEncode('value?=');
-			expect(result).to.equal('dmFsdWU_PQ');
+			assert.equal(result, 'dmFsdWU_PQ');
 		});
 	});
 
 	describe('base64UrlDecode', () => {
 		it('works', () => {
 			const result = base64UrlDecode('dmFsdWU_PQ');
-			expect(result).to.equal('value?=');
+			assert.equal(result, 'value?=');
 		});
 	});
 })
